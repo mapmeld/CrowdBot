8bca70bdd75ed9f8c073a040481ca81ed17789b0
Nick Doiron <ndoiron@mapmeld.com>
2011-12-22 10:38:37 -0500

    updated default programs and reference; data stream optional

--- appengine-server/homepage.py
+++ appengine-server/homepage.py
@@ -17,8 +17,19 @@ class CrowdBotIn(webapp.RequestHandler):
 <html>
 	<head>
 		<title>Watch CrowdBot</title>
+		<script type="text/javascript">
+function init(){
+	if(gup("data") == "show"){
+		livedata();
+	}
+}
+function gup(nm){nm=nm.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");var rxS="[\\?&]"+nm+"=([^&#]*)";var rx=new RegExp(rxS);var rs=rx.exec(window.location.href);if(!rs){return null;}else{return rs[1];}}
+function livedata(){
+	document.getElementById("dataspot").innerHTML = '<iframe src="/crowdbot/out?get=livedata" width="400" height="300" scrolling="yes" frameborder="0" style="border:0px none transparent;"></iframe>';
+}
+		</script>
 	</head>
-	<body>
+	<body onload="init()">
 		<table><tr><td>
 			<h1>CrowdBot Live Streaming</h1>
 			<!-- UStream embed code -->
@@ -32,7 +43,14 @@ class CrowdBotIn(webapp.RequestHandler):
 				<param name="flashvars" value="hostname=www.justin.tv&channel=crowdbot&auto_play=false&start_volume=25" />
 			</object> -->
 			<br/>
-			<iframe src="/crowdbot/out?get=livedata" width="400" height="300" scrolling="yes" frameborder="0" style="border:0px none transparent;"></iframe>
+			<div id="dataspot" style="width:400;height:300px;">
+				<button onclick="livedata()">
+					Show Live Data
+				</button>
+			<!--
+				<iframe src="/crowdbot/out?get=livedata" width="400" height="300" scrolling="yes" frameborder="0" style="border:0px none transparent;"></iframe>
+			-->
+			</div>
 		</td><td>
 			<div>
 				<button onclick="window.location='/crowdbot';" style="font-size:18pt">
@@ -62,16 +80,19 @@ function writeSample(){
 			sketch = "/*\\n  Blink Test\\n  Turns blue and yellow LEDs on and off, repeatedly.\\n  pin 2 is blue\\n  pin 4 is green\\n  activating 2 and 4 together makes aqua-green\\n  pin 6 is yellow\\n  pin 8 is purple\\n  pin 13 is orange indicator on Arduino\\n */\\nvoid setup() {\\n  /* at start, prepare LED pins */\\n  pinMode(2, OUTPUT);\\n  pinMode(4, OUTPUT);\\n  pinMode(6, OUTPUT);\\n  pinMode(8, OUTPUT);\\n  pinMode(13, OUTPUT);\\n}\\nvoid loop() {\\n  /* program a light show here! */\\n  digitalWrite(2, HIGH);\\n  digitalWrite(6, HIGH);\\n  digitalWrite(8,LOW);\\n  /* wait 3 seconds with those settings */\\n  delay(3000);\\n  digitalWrite(2, LOW);\\n  digitalWrite(6, LOW);\\n  digitalWrite(8,HIGH);\\n  /* wait 2 seconds with those settings */\\n  delay(2000);\\n  /* loop() repeats */\\n}";
 			break;
 		case "sensor":
-			sketch = "/* Sensor Test\\n  Turns purple LED on whenever sensor detects the yellow LED\\n  digital pin 6 is yellow\\n  digital pin 8 is purple\\n  pin 12 is light sensor\\n*/\\nint onIn = 1;\\nint lightSense;\\nvoid setup(){\\n  pinMode(6, OUTPUT);\\n  pinMode(8, OUTPUT);\\n}\\nvoid loop(){\\n  // cycle the target light\\n  // target light is on 0.5 seconds out of 2\\n  onIn = onIn + 1;\\n  if(onIn % 5 == 0){\\n    digitalWrite(6, HIGH);\\n    onIn = 1;\\n  }\\n  else{\\n    digitalWrite(6, LOW);\\n  }\\n  lightSense = analogRead(1);\\n  if(lightSense < 800){\\n    // target light detected! purple LED on!\\n    digitalWrite(8, HIGH);\\n  }\\n  else{\\n    // target light off! purple LED off!\\n    digitalWrite(8, LOW);\\n  }\\n  delay(500);\\n}";
+			sketch = "/* Sensor Test\\n  Turns purple LED on whenever sensor detects the red LED\\n  pin 10 is red\\n  digital pin 8 is purple\\n  analog 3 is light sensor\\n*/\\nint onIn = 1;\\nint lightSense;\\nvoid setup(){\\n  pinMode(8, OUTPUT);\\n  pinMode(10, OUTPUT);\\n}\\nvoid loop(){\\n  // cycle the target light\\n  // target light is on 0.5 seconds out of 2\\n  onIn = onIn + 1;\\n  if(onIn % 5 == 0){\\n    digitalWrite(10, HIGH);\\n    onIn = 1;\\n  }\\n  else{\\n    digitalWrite(10, LOW);\\n  }\\n  lightSense = analogRead(3);\\n  if(lightSense > 70){\\n    // target light detected! purple LED on!\\n    digitalWrite(8, HIGH);\\n  }\\n  else{\\n    // target light off! purple LED off!\\n    digitalWrite(8, LOW);\\n  }\\n  delay(500);\\n}";
 			break;
 		case "hello":
 			sketch = "/* Stream Test\\nCrowdBot data stream test\\nsays \\"hello\\" then random numbers */\\nvoid setup(){\\n  Serial.begin(9600);\\n  Serial.println(\\"hello\\");\\n  // read unconnected analog pin 0 for randomizing noise\\n  randomSeed( analogRead(0) );\\n}\\nvoid loop(){\\n  Serial.println( random(1, 101) );\\n  delay(1000);\\n}";
 			break;
 		case "random":
-			sketch = "/* Random Blink\\n  blinks blue, green, and purple for random intervals */\\nvoid setup(){\\n  // prepare blink lights\\n  pinMode(2, OUTPUT);\\n  pinMode(4, OUTPUT);\\n  pinMode(8, OUTPUT);\\n  // read unconnected analog pin 0 for randomizing noise\\n  randomSeed( analogRead(0) );\\n}\\nvoid loop(){\\n  digitalWrite(2, HIGH);\\n  delay( random(750, 2001) );\\n  digitalWrite(2, LOW);\\n  digitalWrite(4, HIGH);\\n  delay( random(750, 2001) );\\n  digitalWrite(4, LOW);\\n  digitalWrite(6, HIGH);\\n  delay( random(750, 2001) );\\n  digitalWrite(6, LOW);\\n  delay( random(750, 1001) );\\n}";
+			sketch = "/* Random Blink\\n  blinks blue, green, and purple for random intervals */\\nvoid setup(){\\n  // prepare blink lights\\n  pinMode(2, OUTPUT);\\n  pinMode(4, OUTPUT);\\n  pinMode(6, OUTPUT);\\n  // read unconnected analog pin 0 for randomizing noise\\n  randomSeed( analogRead(0) );\\n}\\nvoid loop(){\\n  digitalWrite(2, HIGH);\\n  delay( random(750, 2001) );\\n  digitalWrite(2, LOW);\\n  digitalWrite(4, HIGH);\\n  delay( random(750, 2001) );\\n  digitalWrite(4, LOW);\\n  digitalWrite(6, HIGH);\\n  delay( random(750, 2001) );\\n  digitalWrite(6, LOW);\\n  delay( random(750, 1001) );\\n}";
 			break;
 		case "streamer":
-			sketch = "/* Data Streamer\\n  streams light sensor value to CrowdBot\\n  target light is pink LED with variable analog value */\\nint lightSense;\\nint lightCycle = 1;\\nvoid setup(){\\n  pinMode(9, OUTPUT);\\n  Serial.begin(9600);\\n  randomSeed( analogRead(0) );\\n}\\nvoid loop(){\\n  // set the light LED to a random value every 4 seconds\\n  if(lightCycle % 5 == 0){\\n    lightCycle = 1;\\n    analogWrite(9, random(20,250) );\\n  }\\n  else{\\n    lightCycle = lightCycle + 1;\\n  }\\n  // read the light sensor every second\\n  lightSense = analogRead(3);\\n  Serial.println( lightSense );\\n  delay(1000);\\n}";
+			sketch = "/* Data Streamer\\n  streams light sensor value to CrowdBot\\n  target light is red LED with variable analog value */\\nint lightSense;\\nint lightCycle = 1;\\nvoid setup(){\\n  pinMode(10, OUTPUT);\\n  Serial.begin(9600);\\n  randomSeed( analogRead(0) );\\n}\\nvoid loop(){\\n  // set the light LED to a random value every 4 seconds\\n  if(lightCycle % 5 == 0){\\n    lightCycle = 1;\\n    analogWrite(10, random(20,250) );\\n  }\\n  else{\\n    lightCycle = lightCycle + 1;\\n  }\\n  // read the light sensor every second\\n  lightSense = analogRead(3);\\n  Serial.println( lightSense );\\n  delay(1000);\\n}";
+			break;
+		case "servo":
+			sketch = "/* Motor Test */\\n#include <Servo.h>\\nServo myservo;\\nvoid setup(){\\n  myservo.attach(9);\\n  randomSeed( analogRead(0) );\\n}\\nvoid loop(){\\n  myservo.write( random(0, 180) );\\n  delay(5000);\\n}";
 			break;
 		case "current":
 			var s = document.createElement("script");
@@ -86,6 +107,7 @@ function writeSample(){
 function update(currentSketch){
 	while(currentSketch.indexOf("|~|") > -1){
 		currentSketch = currentSketch.replace("|~|","\\n");
+		currentSketch = currentSketch.replace("\\n\\n","\\n")
 	}
 	$("sketchplace").value = currentSketch;
 }
@@ -124,6 +146,7 @@ ul.spacedlist li{
 						<option value="none">-</option>
 						<option value="blink">Blink Test</option>
 						<option value="sensor">Sensor Test</option>
+						<option value="servo">Motor Test</option>
 						<option value="hello">Stream Test</option>
 						<option value="random">Random Blink</option>
 						<option value="streamer">Data Streamer</option>
@@ -142,12 +165,18 @@ ul.spacedlist li{
 				</button>
 				<h3>Reference</h3>
 				<ul class="spacedlist" style="font-family:courier;list-style:none;">
-					<li>analogRead( PIN# )<br/>return pin value from 0 (0V) to 1024 (5V)</li>
-					<li>analogWrite( PIN# , 0 <= val <= 255 )<br/>activates the pin to 0-5V</li>
-					<li>delay( milliseconds )<br/>wait before continuing to the next line</li>
-					<li>digitalRead( PIN# )<br/>return pin as HIGH or LOW</li>
-					<li>digitalWrite( PIN# , HIGH or LOW )<br/>sets the pin to 5V or 0V / GND</li>
-					<li>pinMode( PIN# , INPUT or OUTPUT )<br/>sets a pin to read or write information</li>
+					<li>pinMode( PIN# , INPUT or OUTPUT )<br/>prepares a pin to read or write information</li>
+					<li>digitalWrite( PIN# , HIGH or LOW )<br/>turns the pin on or off / GND</li>
+					<li style="padding-bottom:5px;border-bottom:1px solid silver;">delay( milliseconds )<br/>waits before continuing to the next line</li>
+					
+					<li>analogRead( PIN# )<br/>returns sensor value from 0 (off) to 1024 (full-on)</li>
+					<!-- <li>analogWrite( PIN# , 0 <= val <= 255 )<br/>activates a voltage between 0 and 5V</li> -->
+					<li style="padding-bottom:5px;border-bottom:1px solid silver;">digitalRead( PIN# )<br/>returns sensor value as HIGH or LOW</li>
+					
+					<li>#include &lt;Servo.h&gt;<br/>Servo myservo;<br/>paste at start of code to allow motor control</li>
+					<li>myservo.attach(9);<br/>Prepare servo motor inside setup</li>
+					<li style="padding-bottom:5px;border-bottom:1px solid silver;">myservo.write( 0 <= val <= 179 )<br/>Turn motor to 0-179 degrees</li>
+
 					<li>Serial.begin( 9600 )<br/>connect computer and Arduino over USB/serial port</li>
 					<li>Serial.println( # or "" )<br/>output a message (number or text) to the computer</li>
 					<li>Serial.println("DONE")<br/>start the next program</li>
@@ -172,7 +201,7 @@ ul.spacedlist li{
 			# Twitter name
 			if(self.request.get('data').replace('\n','').replace(' ','').replace('	','') == ''):
 				# no data recorded
-				finished_format = "%s: #CrowdBot ran your sketch, '%s'. No data was returned"
+				finished_format = "%s: #CrowdBot ran your sketch, '%s'. No data was returned."
 			else:
 				# send data record
 				finished_format = "%s: #CrowdBot ran your sketch, '%s'. Data stored at http://mapmeld.com/crowdbot/out?get=feed&id=" + self.request.get('id')
@@ -216,9 +245,13 @@ Data returned by your sketch is stored at http://mapmeld.com/crowdbot/out?get=fe
 		sketch.dosend = self.request.get('dosend')
 		sketch.hasRun = 'False'
 		sketch.put()
-		self.redirect('/crowdbot?screen=watch')
+		# live data stream visible if user's program includes data streaming
+		if((self.request.get('dostream') == 'on') and (self.request.get('mysketch').find('Serial.begin') > -1)):
+			self.redirect('/crowdbot?screen=watch&data=show')
+		else:
+			self.redirect('/crowdbot?screen=watch')
 
-# API requests
+# API requests from the crowdbot-host machine
 class CrowdBotOut(webapp.RequestHandler):
   def get(self):
 	if(self.request.get('get') == 'livecode'):
